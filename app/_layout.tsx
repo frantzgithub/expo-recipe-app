9960464f3cb8206a2a75b4d4809fa2078c11376b
berlin <berlingit@gmail.com>
2024-09-15 12:44:10 -0400

    setup application

---
+++ app/_layout.tsx
@@ -0,0 +1,12 @@
+
+
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function RootLayout() {
+  return (
+    <View>
+      <Text>Layout principal</Text>
+    </View>
+  )
+}
